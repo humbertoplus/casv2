6b01906d6441633d3d9ca2d8fa6e817fa31fe4e9
alexcontreras <alexcontreras@outlook.com>
2013-11-20 15:54:28 -0600

    Ajuste del comentario

--- php/catalogo-cuentas.php
+++ php/catalogo-cuentas.php
@@ -1,5 +1,5 @@
 <?php
-/*~ Archivo plantilla-maestra.php
+/*~ Archivo catalogo-cuentas.php
 .---------------------------------------------------------------------------.
 |    Software: CAS - Computerized Accountancy System                        |
 |     Versión: 1.0                                                          |
