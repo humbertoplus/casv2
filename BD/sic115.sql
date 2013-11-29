0e9ab3cbb8662724026ccf32a54782578c017846
alexcontreras <alexcontreras@outlook.com>
2013-11-28 22:24:56 -0600

    BD actualizada

--- BD/sic115.sql
+++ BD/sic115.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 28-11-2013 a las 16:30:17
+-- Tiempo de generación: 29-11-2013 a las 04:24:04
 -- Versión del servidor: 5.6.12-log
 -- Versión de PHP: 5.4.16
 
@@ -282,7 +282,8 @@ CREATE TABLE IF NOT EXISTS `cuentas` (
 --
 
 INSERT INTO `cuentas` (`codigo_cuenta`, `subgrupo`, `nombre_cuenta`, `tiene_subcuenta`, `descripcion_cuenta`, `saldo_debe`, `saldo_haber`) VALUES
-('1.2.1.1', '1.2.1', 'Materiales Directos', 'Si', NULL, 1600, 0),
+('1.1.1.1', '1.1.1', 'Banco Agrícola', 'No', 'Cuenta para la Sociedad en el Banco Agrícola.', 0, 0),
+('1.2.1.1', '1.2.1', 'Materiales Directos', 'Si', NULL, 0, 0),
 ('1.2.1.2', '1.2.1', 'Materiales Indirectos', 'Si', NULL, 0, 0),
 ('1.2.2.1', '1.2.2', 'Botellas de Vino de Naranja', 'No', NULL, 0, 0),
 ('1.2.2.2', '1.2.2', 'Botellas de Vino de Mandarina', 'No', NULL, 0, 0),
@@ -290,9 +291,10 @@ INSERT INTO `cuentas` (`codigo_cuenta`, `subgrupo`, `nombre_cuenta`, `tiene_subc
 ('1.2.2.4', '1.2.2', 'Botellas de Vino de Marañón', 'No', NULL, 0, 0),
 ('1.2.2.5', '1.2.2', 'Botellas de Vino de Coco', 'No', NULL, 0, 0),
 ('1.2.2.6', '1.2.2', 'Botellas de Vino de Rosa de Jamaica', 'No', NULL, 0, 0),
+('1.3.2.1', '1.3.2', 'Inversión en maquinaria y equipo de producción.', 'No', 'Este rubro comprende lo relacionado con la adquisición de la maquinaria, equipo, instrumentos y utensilios que serán indispensables en los procesos productivos para la elaboración de vinos y que previamente fueron considerados en la sección del diseño det', 422.5, 0),
 ('1.3.5.1', '1.3.5', 'Inversión en Mobiliario y Equipo de Oficina', 'No', 'En este rubro se incluye todo el mobiliario y equipo para la totalidad de las áreas administrativas a considerar para la empresa Vinos Nonualcos y Cia. De C.V.', 0, 0),
-('1.3.6.1', '1.3.6', 'Inversión en Obra Civil', 'No', 'Cuenta para registrar las inversiones relacionadas con el trabajo de Obra Civil.', 0, 0),
-('4.1.1.1', '4.1.1', 'Mano de Obra Directa', 'No', NULL, 0, 1500),
+('1.3.6.1', '1.3.6', 'Inversión en Obra Civil', 'No', 'Cuenta para registrar las inversiones relacionadas con el trabajo de Obra Civil.', 233.9, 0),
+('4.1.1.1', '4.1.1', 'Mano de Obra Directa', 'No', NULL, 0, 0),
 ('4.1.1.2', '4.1.1', 'Costo de Materias Primas', 'No', NULL, 0, 0),
 ('4.1.2.1', '4.1.2', 'Sueldos y Salarios', 'No', NULL, 0, 0),
 ('4.1.2.2', '4.1.2', 'Prestaciones Laborales', 'Si', NULL, 0, 0),
@@ -303,6 +305,37 @@ INSERT INTO `cuentas` (`codigo_cuenta`, `subgrupo`, `nombre_cuenta`, `tiene_subc
 ('4.1.3.4', '4.1.3', 'Internet', 'No', NULL, 0, 0),
 ('4.1.4.1', '4.1.4', 'Depreciación de bienes Muebles', 'No', NULL, 0, 0),
 ('4.1.4.2', '4.1.4', 'Depreciación de Maquinaria y Equipo', 'No', NULL, 0, 0);
+
+--
+-- Disparadores `cuentas`
+--
+DROP TRIGGER IF EXISTS `log_delete_cuentas`;
+DELIMITER //
+CREATE TRIGGER `log_delete_cuentas` AFTER DELETE ON `cuentas`
+ FOR EACH ROW BEGIN
+		INSERT INTO `sic115`.`security_log` (`fecha`, `evento`, `user`, `ip`) 
+		VALUES (CONCAT_WS(' ',CURDATE(), CURTIME()), CONCAT_WS(' ','Se eliminó un registro en la tabla cuentas. El registro eliminado es:',old.codigo_cuenta), USER(), CURRENT_USER());
+    END
+//
+DELIMITER ;
+DROP TRIGGER IF EXISTS `log_insert_cuentas`;
+DELIMITER //
+CREATE TRIGGER `log_insert_cuentas` AFTER INSERT ON `cuentas`
+ FOR EACH ROW BEGIN
+		INSERT INTO `sic115`.`security_log` (`fecha`, `evento`, `user`, `ip`) 
+		VALUES (CONCAT_WS(' ',CURDATE(), CURTIME()), CONCAT_WS(' ','Se insertó un nuevo registro en la tabla cuentas. El valor es:',new.codigo_cuenta), USER(), CURRENT_USER());
+    END
+//
+DELIMITER ;
+DROP TRIGGER IF EXISTS `log_update_cuentas`;
+DELIMITER //
+CREATE TRIGGER `log_update_cuentas` AFTER UPDATE ON `cuentas`
+ FOR EACH ROW BEGIN
+		INSERT INTO `sic115`.`security_log` (`fecha`, `evento`, `user`, `ip`) 
+		VALUES (CONCAT_WS(' ',CURDATE(), CURTIME()), CONCAT_WS(' ','Se modificó un registro en la tabla cuentas.'), USER(), CURRENT_USER());
+    END
+//
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -429,21 +462,62 @@ CREATE TABLE IF NOT EXISTS `registro` (
   `haber` decimal(18,2) DEFAULT NULL,
   `descripcion` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
   `partida_doble` varchar(20) CHARACTER SET utf8 DEFAULT NULL,
+  `justificante` varchar(30) COLLATE latin1_general_ci DEFAULT NULL,
   `fecha_modificacion` date DEFAULT NULL,
   `usuario_creacion` varchar(50) CHARACTER SET utf8 DEFAULT NULL,
   `usuario_modif` varchar(50) CHARACTER SET utf8 DEFAULT NULL,
   `ip` varchar(15) CHARACTER SET utf8 DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=14 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=17 ;
 
 --
 -- Volcado de datos para la tabla `registro`
 --
 
-INSERT INTO `registro` (`id`, `fecha`, `transaccion`, `tipo`, `cuenta`, `concepto`, `debe`, `haber`, `descripcion`, `partida_doble`, `fecha_modificacion`, `usuario_creacion`, `usuario_modif`, `ip`) VALUES
-(11, '2013-11-28', 1, '', '1.2.1.1.1', 'test', '100.00', '0.00', 'eetert', NULL, NULL, 'administrador', NULL, '127.0.0.1'),
-(12, '2013-11-28', 1, 'simple', '1.2.1.1.1', 'test', '1500.00', '0.00', 'test', NULL, NULL, 'administrador', NULL, '127.0.0.1'),
-(13, '2013-11-28', 1, 'simple', '4.1.1.1', 'test', '0.00', '1500.00', 'test', NULL, NULL, 'administrador', NULL, '127.0.0.1');
+INSERT INTO `registro` (`id`, `fecha`, `transaccion`, `tipo`, `cuenta`, `concepto`, `debe`, `haber`, `descripcion`, `partida_doble`, `justificante`, `fecha_modificacion`, `usuario_creacion`, `usuario_modif`, `ip`) VALUES
+(1, '2013-09-02', 1, '', '1.3.6.1', 'Eliminación de Pared', '22.50', '0.00', 'Eliminación de pared. Dimensiones: 2.5m x 3.0m. Área total: 7.5 metros cuadrados. Costo por metro cuadrado: $3.00. Cantidad: 1. Total: $22.50', NULL, '', NULL, 'administrador', NULL, '::1'),
+(2, '2013-09-02', 1, '', '1.3.6.1', 'Emparejamiento de suelo', '15.60', '0.00', 'Emparejamiento de suelo. Área total: 7.8 metros cuadrados. Costo por metro cuadrado: $2.00. Cantidad: 1. Total: 15.60', NULL, '', NULL, 'administrador', NULL, '::1'),
+(3, '2013-09-02', 1, '', '1.3.6.1', 'División Comedor-Producción', '12.75', '0.00', 'División Comedor-Producción (1.7m x 3.0m). Área total: 5.1 metros cuadrados. Costo por metro cuadrado: $2.50. Cantidad: 1. Total: 12.75', NULL, '', NULL, 'administrador', NULL, '::1'),
+(4, '2013-09-02', 1, '', '1.3.6.1', 'División Patio-Producción', '15.00', '0.00', 'División Patio-Producción (3.5m x 3.0m). Área total: 6.0 metros cuadrados. Costo por metro cuadrado: $2.50. Cantidad: 1. Total: $26.75', NULL, '', NULL, 'administrador', NULL, '::1'),
+(5, '2013-09-02', 1, '', '1.3.6.1', 'División Producto en Proceso', '26.25', '0.00', 'División Producto en Proceso (3.5m x 3.0m). Área total: 10.5 metros cuadrados. Costo por metro cuadrado: $2.50. Total: $26.25', NULL, '', NULL, 'administrador', NULL, '::1'),
+(6, '2013-09-02', 1, '', '1.3.6.1', 'Divisiones', '20.00', '0.00', 'Divisiones (2m x 2m). Área total: 4.0 metros cuadrados. Costo por metro cuadrado: $2.50. Total: $20.00', NULL, '', NULL, 'administrador', NULL, '::1'),
+(7, '2013-09-02', 1, '', '1.3.6.1', 'Divisiones', '16.50', '0.00', 'Test', NULL, NULL, NULL, 'administrador', NULL, '::1'),
+(9, '2013-09-03', 1, '', '1.3.6.1', 'Puertas', '105.30', '0.00', 'Test', NULL, NULL, NULL, 'administrador', NULL, '::1'),
+(10, '2013-09-04', 2, '', '1.3.2.1', 'Pelador de naranjas', '48.00', '0.00', '4 peladores a $12.00 c/u', NULL, NULL, NULL, 'administrador', NULL, '::1'),
+(11, '2013-09-04', 2, '', '1.3.2.1', 'Pelador de Cítricos', '12.00', '0.00', '4 peladores a $3.00 c/u', NULL, NULL, NULL, 'administrador', NULL, '::1'),
+(12, '2013-09-04', 2, '', '1.3.2.1', 'Pelador y cortador de Piña', '20.00', '0.00', '4 unidades a $5.00 c/u', NULL, NULL, NULL, 'administrador', NULL, '::1'),
+(13, '2013-09-04', 2, '', '1.3.2.1', 'Cocina eléctrica para Rosa de Jamaica', '80.00', '0.00', '2 unidades a $40.00 c/u', NULL, NULL, NULL, 'administrador', NULL, '::1'),
+(14, '2013-09-04', 2, '', '1.3.2.1', 'Olla para hervir', '14.00', '0.00', '2 unidades a $7.00 c/u.', NULL, NULL, NULL, 'administrador', NULL, '127.0.0.1'),
+(15, '2013-09-06', 2, '', '1.3.2.1', 'Exprimidor/Prensa de cítricos', '132.00', '0.00', '4 unidades a $33.00 c/u.', NULL, NULL, NULL, 'administrador', NULL, '127.0.0.1'),
+(16, '2013-09-06', 2, '', '1.3.2.1', 'Encorchadora', '116.50', '0.00', '1 unidad a $116.50', NULL, NULL, NULL, 'administrador', NULL, '127.0.0.1');
+
+--
+-- Disparadores `registro`
+--
+DROP TRIGGER IF EXISTS `log_delete_registro`;
+DELIMITER //
+CREATE TRIGGER `log_delete_registro` AFTER DELETE ON `registro`
+ FOR EACH ROW BEGIN
+		INSERT INTO `sic115`.`security_log` (`fecha`, `evento`, `user`, `ip`) VALUES (CONCAT_WS(' ',CURDATE(), CURTIME()), CONCAT_WS(' ','Se eliminó un registro en la tabla registro.'), USER(), CURRENT_USER());
+    END
+//
+DELIMITER ;
+DROP TRIGGER IF EXISTS `log_insert_registro`;
+DELIMITER //
+CREATE TRIGGER `log_insert_registro` AFTER INSERT ON `registro`
+ FOR EACH ROW BEGIN
+		INSERT INTO `sic115`.`security_log` (`fecha`, `evento`, `user`, `ip`) VALUES (CONCAT_WS(' ',CURDATE(), CURTIME()), CONCAT_WS(' ','Se insertó un nuevo registro en la tabla registro.'), USER(), CURRENT_USER());
+    END
+//
+DELIMITER ;
+DROP TRIGGER IF EXISTS `log_update_registro`;
+DELIMITER //
+CREATE TRIGGER `log_update_registro` AFTER UPDATE ON `registro`
+ FOR EACH ROW BEGIN
+		INSERT INTO `sic115`.`security_log` (`fecha`, `evento`, `user`, `ip`) VALUES (CONCAT_WS(' ',CURDATE(), CURTIME()), CONCAT_WS(' ','Se actualizó un registro en la tabla registro.'), USER(), CURRENT_USER());
+    END
+//
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -458,16 +532,14 @@ CREATE TABLE IF NOT EXISTS `security_log` (
   `user` varchar(50) COLLATE latin1_general_ci NOT NULL,
   `ip` varchar(20) COLLATE latin1_general_ci NOT NULL,
   PRIMARY KEY (`id_evento`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=2 ;
 
 --
 -- Volcado de datos para la tabla `security_log`
 --
 
 INSERT INTO `security_log` (`id_evento`, `fecha`, `evento`, `user`, `ip`) VALUES
-(1, '2013-11-27 08:24:30', 'Se insertó un nuevo registro en la tabla subcuentas. El valor es: 1.1.1.1.1', 'root@localhost', 'root@localhost'),
-(2, '2013-11-27 08:25:06', 'Se insertó un nuevo registro en la tabla subcuentas. El valor es: 1.1.1.1.2', 'root@localhost', 'root@localhost'),
-(3, '2013-11-27 08:47:19', 'Se insertó un nuevo registro en la tabla subcuentas. El valor es: 1.1.1.1.1', 'root@localhost', 'root@localhost');
+(1, '2013-11-28 22:06:53', 'Se insertó un nuevo registro en la tabla cuentas. El valor es: 1.1.1.1', 'root@localhost', 'root@localhost');
 
 -- --------------------------------------------------------
 
@@ -491,7 +563,7 @@ CREATE TABLE IF NOT EXISTS `subcuentas` (
 --
 
 INSERT INTO `subcuentas` (`codigo_subcuenta`, `cuenta`, `nombre_subcuenta`, `descripcion`, `saldo_debe`, `saldo_haber`) VALUES
-('1.2.1.1.1', '1.2.1.1', 'Naranja', 'Cuenta para registrar los movimientos del activo "Naranja"', 1600, 0),
+('1.2.1.1.1', '1.2.1.1', 'Naranja', 'Cuenta para registrar los movimientos del activo "Naranja"', 0, 0),
 ('1.2.1.1.2', '1.2.1.1', 'Mandarina', 'Cuenta para registrar los movimientos del activo "Mandarina"', 0, 0),
 ('1.2.1.1.3', '1.2.1.1', 'Piña', 'Cuenta para registrar los movimientos del activo "Piña"', 0, 0),
 ('1.2.1.1.4', '1.2.1.1', 'Marañón', 'Cuenta para registrar los movimientos del activo "Marañón"', 0, 0),
@@ -508,12 +580,30 @@ INSERT INTO `subcuentas` (`codigo_subcuenta`, `cuenta`, `nombre_subcuenta`, `des
 --
 -- Disparadores `subcuentas`
 --
+DROP TRIGGER IF EXISTS `log_delete_subcuentas`;
+DELIMITER //
+CREATE TRIGGER `log_delete_subcuentas` AFTER DELETE ON `subcuentas`
+ FOR EACH ROW BEGIN
+		INSERT INTO `sic115`.`security_log` (`fecha`, `evento`, `user`, `ip`) 
+		VALUES (CONCAT_WS(' ',CURDATE(), CURTIME()), CONCAT_WS(' ','Se eliminó un registro en la tabla subcuentas. El registro eliminado es:',old.codigo_subcuenta), USER(), CURRENT_USER());
+    END
+//
+DELIMITER ;
 DROP TRIGGER IF EXISTS `log_insert_subcuentas`;
 DELIMITER //
 CREATE TRIGGER `log_insert_subcuentas` AFTER INSERT ON `subcuentas`
  FOR EACH ROW BEGIN
 		INSERT INTO `sic115`.`security_log` (`fecha`, `evento`, `user`, `ip`) 
 		VALUES (concat_ws(' ',CURDATE(), curtime()), concat_WS(' ','Se insertó un nuevo registro en la tabla subcuentas. El valor es:',new.codigo_subcuenta), user(), current_user());
+    END
+//
+DELIMITER ;
+DROP TRIGGER IF EXISTS `log_update_subcuentas`;
+DELIMITER //
+CREATE TRIGGER `log_update_subcuentas` AFTER UPDATE ON `subcuentas`
+ FOR EACH ROW BEGIN
+		INSERT INTO `sic115`.`security_log` (`fecha`, `evento`, `user`, `ip`) 
+		VALUES (CONCAT_WS(' ',CURDATE(), CURTIME()), CONCAT_WS(' ','Se modificó un registro en la tabla subcuentas.'), USER(), CURRENT_USER());
     END
 //
 DELIMITER ;
